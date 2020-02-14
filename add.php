3755e1980557a1062e0ef31e06651a8152f08410
Mats <99052518@mydavinci.nl>
2020-02-14 16:23:16 +0100

    insert

--- add.php
+++ add.php
@@ -1,6 +1,9 @@
-<?php
-
-echo $_POST["titel"];
-echo $_POST["beschrijving"];
-
+<?php 
+  include 'connect.php';
+  $sql = "INSERT INTO `todos` (titel, beschrijving) VALUES ('$_POST[titel]', '$_POST[beschrijving]')";
+  if ($conn->query($sql) === TRUE) {
+    header("location: index.php");
+    exit();
+  }
+$conn->close();
 ?>
