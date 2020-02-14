0f029d1bb2c700eb72f3884e1e2cb6e6723ab12f
Mats <99052518@mydavinci.nl>
2020-02-14 16:52:05 +0100

    changed edit button

--- card.php
+++ card.php
@@ -1,9 +1,14 @@
     <div class="col-md-12" id="<?=$id?>">
       <div class="jumbotron">
       <button id="<?=$id?>" type="button" class="buttona btn btn-danger">Delete</button>
+      <button id="<?=$id?>" type="button" class="buttonb btn btn-primary">Edit</button>
+      <button id="<?=$id?>" type="button" class="editBtn btn btn-success">Save</button>
+      <button id="<?=$id?>" type="button" class="editCancel btn btn-danger">Cancel</button>
         <div class="container">
-          <h1 class="display-3"><?=$titel?></h1>
-          <p class="lead"><?=$beschrijving?></p>
+          <h1 class="display-3 dataText" id="titel-<?=$id?>"><?=$titel?></h1>
+          <input type="text" class="editField" id="titeledit<?=$id?>" value="">
+          <p class="lead dataText" id="beschrijving-<?=$id?>"><?=$beschrijving?></p>
+          <input type="text" class="editField" id="beschrijvingedit<?=$id?>" value="">
           <ul class="list-group">
             <li class="list-group-item">Morbi leo risus</li>
             <li class="list-group-item">Porta ac consectetur ac</li>
--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 $(function () {
-  $("#noteEdit").hide()
-  $("#newNote").on("click", function() {
-    $("#noteEdit").show()
+  $(".editField").hide();
+  $(".editBtn").hide();
+  $("#noteEdit").hide();
+  $(".editCancel").hide();
+  $("#newNote").on("click", function () {
+    $("#noteEdit").show();
   })
 
-  $(".buttona").on("click", function() {
-    data = { 'id': this.id }
+  $(".buttona").on("click", function () {
+    data = {
+      'id': this.id
+    }
     $.post('delete.php', data, function (response) {
-      alert("action performed successfully");
+      window.location = 'index.php'
     });
   })
-});
 
+  $(".editCancel").on("click", function () {
+    $(this).hide();
+    $(".editBtn").hide();
+    $(".buttonb").show();
+    $(".dataText").show();
+    $(".editField").hide();
+  })
+
+  $(".buttonb").on("click", function () {
+    $(this).hide();
+    $(".dataText").hide()
+    $(".editField").show();
+    $(".editCancel").show();
+    $(".editBtn").show();
+    $(`#titeledit${this.id}`).val($(`#titel-${this.id}`).text())
+    $(`#beschrijvingedit${this.id}`).val($(`#beschrijving-${this.id}`).text())
+  })
+});
